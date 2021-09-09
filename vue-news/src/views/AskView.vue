ab1c0a957b662acf4637da79e9067be903feea13
ohhyeun kwon <kdhgus92@gmail.com>
2021-09-10 00:29:33 +0900

    lol

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,28 +1,14 @@
 <template>
   <div>
     ask
-    <div v-for="ask in asks">{{ ask.title }}</div>
+    <div v-for="item in this.$store.state.ask">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index.js';
 export default {
-  data() {
-    return {
-      asks: [],
-    };
-  },
   created() {
-    var vm = this;
-    fetchAskList()
-      .then(function(response) {
-        console.log(response);
-        vm.asks = response.data;
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+    this.$store.dispatch('FETCH_ASK');
   },
 };
 </script>
