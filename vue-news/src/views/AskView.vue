fbc2ee4de30f375130cc4baca8c41ddfda0ad92a
Ohhyeun Kwon <35871896+kdhgus92@users.noreply.github.com>
2025-02-16 18:03:58 +0900

    comment remove

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -6,18 +6,10 @@
 
 <script>
 import ListItem from "../components/ListItem.vue";
-// import { mapGetters } from "vuex";
-// import { mapState } from "vuex";
 
 export default {
   components: {
     ListItem,
   },
-  // computed: {
-  //   ...mapGetters(["fetchedAsk"]),
-  // ...mapState({
-  //   fetchedAsk: state => state.ask
-  // })
-  // },
 };
 </script>
